ec2530f758a9ea5c7d2c217bb86e868e5ee76380
GDKobrae <djedjegboble.pro@gmail.com>
2023-04-23 18:43:02 +0200

    Pushing event creation page

--- ticketing/events/create-modify-delete-events.php
+++ ticketing/events/create-modify-delete-events.php
@@ -8,5 +8,26 @@
 </head>
 <body>
     <h1>Créer-Modifier-Annuler un événement</h1>
+
+    <form method="POST">
+        <label for="event-name">Nom de l'événement: </label>
+        <input type="text" id="event-name" name="event-name">
+
+        <br>
+
+        <label for="event-place">Lieux de l'évènement: </label>
+        <input type="text" id="event-place" name="event-place">
+
+        <br>
+
+        <label for="event-date">Date de l'évènement: </label>
+        <input type="date" id="event-date" name="event-date">
+
+        <br>
+
+        <label for="event-description">Description de l'évènement: </label>
+        <textarea name="event-description" id="event-description" cols="30" rows="5"></textarea>
+
+    </form>
 </body>
 </html>
