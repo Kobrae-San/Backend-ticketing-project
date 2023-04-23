1dad73441231576845635663e5e42851c9023372
GDKobrae <djedjegboble.pro@gmail.com>
2023-04-23 19:38:19 +0200

    Modifying create event page

--- ticketing/events/create-modify-delete-events.php
+++ ticketing/events/create-modify-delete-events.php
@@ -1,4 +1,20 @@
-<!DOCTYPE html>
+<?php
+    session_start();
+    require '../../inc/pdo.php';
+    if (!isset($_GET["your_token"])) {
+        header('Location: ../dashboard.php');
+    }
+    $username = $_GET['username'];
+    $verify_token = $auth_pdo->prepare("
+        SELECT * FROM users
+        WHERE login :login
+    ");
+    
+    $verify_token->execute([
+        ":login" => $username
+    ]);
+
+?><!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -8,5 +24,26 @@
 </head>
 <body>
     <h1>Créer-Modifier-Annuler un événement</h1>
+
+    <form method="POST">
+        <label for="event-name">Nom de l'événement: </label>
+        <input type="text" id="event-name" name="event-name" required>
+
+        <br>
+
+        <label for="event-place">Lieux de l'évènement: </label>
+        <input type="text" id="event-place" name="event-place" required>
+
+        <br>
+
+        <label for="event-date">Date de l'évènement: </label>
+        <input type="date" id="event-date" name="event-date" required>
+
+        <br>
+
+        <label for="event-description">Description de l'évènement: </label>
+        <textarea name="event-description" id="event-description" cols="30" rows="5" required></textarea>
+
+    </form>
 </body>
 </html>
