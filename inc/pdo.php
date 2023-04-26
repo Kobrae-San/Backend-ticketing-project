f12ee4bc1b1c60609785f4f4529309890de6d2f9
ShaynnaRmd <129972913+ShaynnaRmd@users.noreply.github.com>
2023-04-26 18:15:34 +0200

    putting pdo back in order

--- inc/pdo.php
+++ inc/pdo.php
@@ -4,7 +4,7 @@
     $auth_engine = "mysql";
     // Hôte : localhost
     $host = "localhost";
-    // Port : 3306 (par défaut pour MySQL, avec MAMP macOS c'est 8889)
+    Port : 3306 //(par défaut pour MySQL, avec MAMP macOS c'est 8889)
     $auth_port = 8889; // port MAMP
     // Nom de la BDD (facultatif) : sakila
     $auth_bdd = "authentification";
@@ -12,7 +12,7 @@
     // Nom d'utilisateur : root
     $user = "root";
     // Mot de passe : 
-    $password_bdd = "root";
+    $password_bdd = "";
 
     $auth_dsn = "$auth_engine:host=$host:$auth_port;dbname=$auth_bdd";
     $auth_pdo = new PDO($auth_dsn, $user, $password_bdd);
