e4387f11ef1ee2e53bd42cc9b3779b9131263a11
william <williams_wandji@yahoo.fr>
2023-04-26 10:58:45 +0200

    Commit

--- ticketing/events/create-modify-delete-events.php
+++ ticketing/events/create-modify-delete-events.php
@@ -22,6 +22,7 @@ if (isset($_GET["your_token"])){
                 <a href="create-event.php?your_token=<?= $hashed ?>&username=<?= $_GET['username'] ?>"><li>Créer un évènement</li></a>
                 <a href="modify-event.php?your_token=<?= $hashed ?>&username=<?= $_GET['username'] ?>"><li>Modifier évènement</li></a>
                 <a href="delete-event.php?your_token=<?= $hashed ?>&username=<?= $_GET['username'] ?>"><li>Supprimer un évènement</li></a>
+                <a href="../../dashboard.php?your_token=<?= $hashed ?>&username=<?= $_GET['username'] ?>">Retour au menu principl</a>
             </ul>
         </nav>
     </header>
