65651970fa2a2d56a5cb09543dff112d4e9a935a
william <williams_wandji@yahoo.fr>
2023-04-27 10:55:35 +0200

    add

--- ticketing/tickets/show-tickets.php
+++ ticketing/tickets/show-tickets.php
@@ -28,34 +28,50 @@
     <title>Modifier évènement</title>
     <link rel="stylesheet" href="../style.css">
 </head>
-        <h2>Billetterie</h2>
-        <?php if ($method == "POST"):
-            $last_name = filter_input(INPUT_POST, "lastname");
-            $first_name = filter_input(INPUT_POST, "firstname");
-            $ticket_id = filter_input(INPUT_POST, "private-ticket-id");
-        ?>
-        <div>
-            <p><?= $last_name ?></p>
-            <p><?= $first_name ?></p>
-             
-        </div>
-        <?php else: ?>
-        <div>
-            <h2>Afficher un billet</h2>
-            <form method="POST">
-                <label for="lastname">Nom: </label>
-                <input type="text" id="lastname" name="lastname" placeholder="Indiquez votre nom de famille" required>
-                
-                <label for="lastname">Prénom: </label>
-                <input type="text" id="firstname" name="firstname" placeholder="Indiquez votre prénom" required>
+    <h2>Billetterie</h2>
+    <?php if ($method == "POST"){
+        $last_name = filter_input(INPUT_POST, "lastname");
+        $first_name = filter_input(INPUT_POST, "firstname");
+        $ticket_id = filter_input(INPUT_POST, "private-ticket-id");
+        
+        require 'vendor/autoload.php'; // Inclure l'autoloader de Composer
 
-                <label for="private-ticket-id">ID privé du Billet</label>
-                <input type="text" id="private-ticket-id" name="private-ticket" placeholder="Renseignez l'identifiant privé"
-                 required>
+        // Importer les classes nécessaires du package bacon/bacon-qr-code avec des alias
+        $pngRendererClass = 'BaconQrCode\Renderer\Image\Png';
+        $writerClass = 'BaconQrCode\Writer';
+        
+        // Créer une nouvelle instance du rendu PNG
+        $pngRenderer = new $pngRendererClass();
+        
+        // Créer une nouvelle instance de l'écrivain de QR code
+        $writer = new $writerClass($pngRenderer);
+        
+        // Générer le QR code
+        $text = 'lastname, firstname, private-ticket-id ';
+        $qrCode = $writer->writeString($text);
+        
+        // Afficher l'image du QR code
+        header('Content-Type: image/png');
+        echo $qrCode;
+        
+    }
+ else{ ?>
+    <div>
+        <h2>Afficher un billet</h2>
+        <form method="POST">
+            <label for="lastname">Nom: </label>
+            <input type="text" id="lastname" name="lastname" placeholder="Indiquez votre nom de famille" required>
+            
+            <label for="lastname">Prénom: </label>
+            <input type="text" id="firstname" name="firstname" placeholder="Indiquez votre prénom" required>
 
-                <input class="submit" type="submit" value="Afficher le billet">
-            </form>
-        </div>
-        <?php endif; ?>
-    </body>
-    </html>
+            <label for="private-ticket-id">ID privé du Billet</label>
+            <input type="text" id="private-ticket-id" name="private-ticket" placeholder="Renseignez l'identifiant privé"
+                required>
+
+            <input class="submit" type="submit" value="Afficher le billet">
+        </form>
+    </div>
+    <?php }?>
+</body>
+</html>
