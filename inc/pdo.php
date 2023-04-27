a8bd16ef7fcf63351cea71aacf7f45f4b0f1e3a0
shay <shaynna.raymond@gmail.com>
2023-04-27 15:03:03 +0200

    changements pdo

--- inc/pdo.php
+++ inc/pdo.php
@@ -6,14 +6,15 @@
     $host = "localhost";
 
     // Port : 3306 (par défaut pour MySQL, avec MAMP macOS c'est 8889)
-    $auth_port = 3306; // port MAMP
+    $auth_port = 3306; // port XAMP
+    $mamp_auth_port = 8888; // port MAMP
     // Nom de la BDD (facultatif) : sakila
     $auth_bdd = "authentification";
     $ticket_bdd = "billeterie";
     // Nom d'utilisateur : root
     $user = "root";
     // Mot de passe : 
-    $password_bdd = "";
+    $password_bdd = "RAYMONDShaynna2001";
 
     $auth_dsn = "$auth_engine:host=$host:$auth_port;dbname=$auth_bdd";
     $auth_pdo = new PDO($auth_dsn, $user, $password_bdd);
