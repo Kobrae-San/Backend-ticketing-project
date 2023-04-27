ea04937b637da85b80cea61a14b14bbe358175aa
Mimenee <imene.toudeft@hotmail.com>
2023-04-27 15:58:02 +0200

    almost finished show-ticket.php and created ticket-details.php

--- ticketing/tickets/show-tickets.php
+++ ticketing/tickets/show-tickets.php
@@ -13,15 +13,19 @@
 
     $method = filter_input(INPUT_SERVER, "REQUEST_METHOD");
 
-    $title = "Afficher un billet";
+    $title = "Afficher billet";
     $website_part = "Billetterie";
+    
+    $erreur = "";
+    $ticket = false;
 
     if ($method == "POST"){
-        $last_name = filter_input(INPUT_POST, "lastname");
-        $first_name = filter_input(INPUT_POST, "firstname");
+        $last_name = filter_input(INPUT_POST, "last-name");
+        $first_name = filter_input(INPUT_POST, "first-name");
         $private_key = filter_input(INPUT_POST, "private-key");
+
         if ($last_name && $first_name && $private_key) {
-            $display_ticket_request = $ticket_pdo -> prepare('
+            $check_ticket_request = $ticket_pdo -> prepare('
             SELECT private_key, last_name, first_name, event_name,
             event_place, event_date, event_description, current_date 
             FROM tickets
@@ -30,56 +34,73 @@
             WHERE private_key = :private_key
             AND last_name = :last_name
             AND first_name = :first_name
-            AND event_name = :event_name
-            AND event_place = :event_place
-            AND event_date = :event_date
-            AND event_description = :event_description
-            AND current_date = :current_date
             ');
 
-            $display_ticket_request -> execute([
+            $check_ticket_request -> execute([
                 ":private_key" => $private_key,
                 ":last_name" => $last_name,
                 ":first_name" => $first_name,
-                ":event_name" => $event_name,
-                ":event_place" => $event_place,
-                ":event_date" => $event_date,
-                ":event_description" => $event_description,
-                ":current_date" => $current_date
             ]);
 
-            $display_ticket = $display_ticket_request -> fetch(PDO::FETCH_ASSOC);
-            if ($display_ticket) {
-                
+            $ticket_info = $check_ticket_request -> fetch(PDO::FETCH_ASSOC);
+
+            if ($ticket_info) {
+                $ticket = true;
+                $event_name = $ticket_info['event_name'];
+                $event_place = $ticket_info['event_place'];
+                $event_date = $ticket_info['event_date'];
+                $event_description = $ticket_info['event_description'];
+                $current_date = $ticket_info['current_date'];
+                //header('Location: ./show-tickets-succes.php');
+            } else {
+                $erreur = "Billet non disponible. Veuillez vérifier les informations fournies puis réessayez";
             }
         } else {
             $erreur = "Champs invalides ou manquants.";
         }
     }
 
-?>
+?><!DOCTYPE html>
 
-        <?php include '../../inc/tpl/header.php'; ?>
-        
-        <div>
-            <p><?= $last_name ?></p>
-            <p><?= $first_name ?></p>
-             
-        </div>
-        <div>
-            <h2>Afficher mon billet</h2>
-            <form method="POST">
-                <label for="lastname">Nom: </label>
-                <input type="text" id="lastname" name="lastname" placeholder="Indiquez votre nom de famille" required>
-                
-                <label for="lastname">Prénom: </label>
-                <input type="text" id="firstname" name="firstname" placeholder="Indiquez votre prénom" required>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title ?></title>
+    <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+    <div>
+        <?php if ($erreur != null): ?>
+            <p><?= $erreur ?></p>
+        <?php endif; ?>
 
-                <label for="private-ticket-id">ID privé du Billet</label>
-                <input type="text" id="private-ticket-id" name="private-key" required>
+        <h2>Afficher un billet</h2>
+        <form method="POST">
+            <label for="last-name">Nom: </label>
+            <input type="text" id="last-name" name="last-name" placeholder="Indiquez votre nom de famille" required>
+            <br>
+            <label for="first-name">Prénom: </label>
+            <input type="text" id="first-name" name="first-name" placeholder="Indiquez votre prénom" required>
+            <br>
+            <label for="private-ticket-id">ID privé du Billet</label>
+            <input type="text" id="private-ticket-id" name="private-key" required>
+            <br>
+            <input type="submit" value="Afficher">
+        </form>
 
-                <input type="submit" value="Afficher mon billet">
-            </form>
-        </div>
-    </body>
-    </html>
+        <?php
+        if ($ticket == true){
+            echo "<p>Nom : ".$ticket_info['last_name']."</p>";
+            echo "<p>Prénom : ".$ticket_info['first_name']."</p>";
+            echo "<p>Evènement : ".$ticket_info['event_name']."</p>";
+            echo "<p>Lieu : ".$ticket_info['event_place']."</p>";
+            echo "<p>Date : ".$ticket_info['event_date']."</p>";
+            echo "<p>Description : ".$ticket_info['event_description']."</p>";
+            echo "<p>Date de génération du billet : ".$ticket_info['current_date']."</p>";
+        }
+        ?>
+    </div>
+</body>
+</html>
