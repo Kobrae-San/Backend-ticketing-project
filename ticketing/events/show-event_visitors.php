9dc250323c6b581effeff1ead7cce3d381679c19
victan78 <116087124+Victan78@users.noreply.github.com>
2023-04-28 16:46:33 +0200

    trfyguh

--- ticketing/events/show-event_visitors.php
+++ ticketing/events/show-event_visitors.php
@@ -27,7 +27,28 @@ session_start();
     ");
     $requete->execute();
     $events = $requete->fetchAll(PDO::FETCH_ASSOC);
-
+    //requete pour recuperer le nom de levenement cliquer
+   if(isset($_GET["id"])){
+    $requete2 = $ticket_pdo->prepare("
+    SELECT * FROM events
+    WHERE id = :id
+    ");
+    $requete2->execute([
+        ":id" => $_GET["id"]
+    ]);
+    $event = $requete2->fetch(PDO::FETCH_ASSOC);
+    //requete pour recuperer les inscrits a levenement cliquer
+    $requete3 = $ticket_pdo->prepare("
+    SELECT * FROM users
+    INNER JOIN tickets
+    ON users.id = tickets.user_id
+    WHERE event_id = :id
+    ");
+    $requete3->execute([
+        ":id" => $_GET["id"]
+    ]);
+    $users = $requete3->fetchAll(PDO::FETCH_ASSOC);}
+    
 ?>
 
    
@@ -35,14 +56,54 @@ session_start();
 
   
  
-?><!DOCTYPE html>
+
+<!DOCTYPE html>
 
 <html>
 <head>
     <meta charset="utf-8">
     <title>Visualiser les événements et leurs inscrits</title>
-    <link rel="stylesheet" href="../style.css">
+    
+    <style>
+        body {
+            background-color: white;
+            font-family: sans-serif;
+        }
+        table {
+  border-collapse: collapse;
+  width: 100%;
+  margin: 0 auto;
+  background-color: #fff;
+}
+
+thead {
+  background-color: #91c788;
+  color: #fff;
+}
+
+th, td {
+  padding: 12px;
+  text-align: left;
+}
+
+th {
+  font-size: 18px;
+  font-weight: bold;
+}
+
+tbody tr:nth-child(even) {
+  background-color: #f2f2f2;
+}
+
+tbody tr:hover {
+  background-color: #ddd;
+}
+
+
+
+    </style>
 </head>
+
 <body class="liste_event">
         
     
@@ -63,8 +124,11 @@ session_start();
                 <tr>
                     <td><?= $event['event_name'] ?></td>
                     <td><?= $event['event_place'] ?></td>
-                    <td><?= $event['event_description'] ?></td>
+                
+                    <td><?= substr($event['event_description'], 0, 50)."..." ?></td>
+                    
                     <td><?= $event['id'] ?></td>
+                    <td><a href="show-event_visitors.php?id=<?= $event['id'] ?>">Voir les inscrits</a></td>
                 </tr>
                 <?php endforeach; ?>
             </tbody>
